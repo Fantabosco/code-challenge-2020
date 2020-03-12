0264362f45ab0881bd6538e2ab84725b5eddcc96
giovannipessiva <rpt@altervista.org>
2020-03-12 20:31:20 +0100

    Solver

--- src/main/java/it/reply/challenge/Main.java
+++ src/main/java/it/reply/challenge/Main.java
@@ -1,6 +1,7 @@
 package it.reply.challenge;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import it.reply.challenge.model.Developer;
@@ -24,9 +25,17 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		challenge(INPUT_A);
+		challenge(INPUT_B);
+		challenge(INPUT_C);
+		challenge(INPUT_D);
+		challenge(INPUT_E);
+		challenge(INPUT_F);
+	}
+	
+	public static void challenge(String dataset) {
 
 		// Reader
-		String dataset = INPUT_A;
 		List<String> file = FileUtils.readFile("in/" + dataset);
 		
 		// Parser
@@ -114,8 +123,155 @@ public class Main {
 	}
 
 	private static int solver(List<Developer> devs, List<Manager> mans, CellEnum[][] map, Dipendente[][] soluzione) {
-		int score = 0;
+	
+		// Ordina per compagnia, così probabilmente li metto vicini e ho bonus
+		List<Developer> devsSorted = new ArrayList<>(devs);
+		List<Manager> mansSorted = new ArrayList<>(mans);
+		
+		// Li posiziono nel primo modo possibile
+		boolean finitiDev = false;
+		boolean finitiMag = false;
+		
+		for(int i = 0; i<map.length && !finitiDev && !finitiMag; i++) {
+			for(int j = 0; j<map[i].length && !finitiDev && !finitiMag; j++) {
+				switch(map[i][j]) {
+				case DEV:
+					Developer d = findBestDev(devsSorted, soluzione, j, i);
+					if(d != null) {
+						d.flagPosizionato = true;
+						d.x = j;
+						d.y = i;
+						soluzione[i][j] = d;
+					} else {
+						finitiDev = true;
+					}
+					break;
+				case MAN:
+					Manager m = findBestMan(mansSorted, soluzione, j, i);
+					if(m != null) {
+						m.flagPosizionato = true;
+						m.x = j;
+						m.y = i;
+						soluzione[i][j] = m;
+					} else {
+						finitiMag = true;
+					}
+					break;
+				}
+			}
 
-		return score;
+		}		
+	
+		return 0;
 	}
+	
+	private static Developer findBestDev(List<Developer> devs, Dipendente[][] soluzione, int x, int y) {
+		List<Dipendente> dips = new ArrayList<>();
+		try {
+			dips.add(soluzione[x-1][y]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x+1][y]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x][y-1]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x][y+1]);
+		} catch(Exception e) {}
+		dips.removeAll(Collections.singleton(null));
+		if(dips.isEmpty()) {
+			return devs.get(0);
+		}
+		int max = 0;
+		Developer best = null;
+		for(Developer d1 : devs) {
+			if(!d1.flagPosizionato) {
+				int sum = 0;
+				for(Dipendente d2 : dips) {
+					sum += calcolaTP(d1, d2);
+				}
+				if(sum > max) {
+					best = d1;
+				}
+			}
+		}
+		return best;
+	}
+	
+	private static Manager findBestMan(List<Manager> mans, Dipendente[][] soluzione, int x, int y) {
+		List<Dipendente> dips = new ArrayList<>();
+		try {
+			dips.add(soluzione[x-1][y]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x+1][y]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x][y-1]);
+		} catch(Exception e) {}
+		try {
+			dips.add(soluzione[x][y+1]);
+		} catch(Exception e) {}
+		dips.removeAll(Collections.singleton(null));
+		if(dips.isEmpty()) {
+			return mans.get(0);
+		}
+		int max = 0;
+		Manager best = null;
+		for(Manager d1 : mans) {
+			if(!d1.flagPosizionato) {
+				int sum = 0;
+				for(Dipendente d2 : dips) {
+					sum += calcolaTP(d1, d2);
+				}
+				if(sum > max) {
+					best = d1;
+				}
+			}
+		}
+		return best;
+	}
+
+	/**
+	 * Calcola il work potential
+	 */
+	private static int calcolaWP(Developer d1, Developer d2) {
+		List<String> a;
+		List<String> b;
+
+		a = new ArrayList<>(d1.skills);
+		b = new ArrayList<>(d2.skills);
+		a.removeAll(b);
+		int sharedSkills = d1.skills.size() - a.size();
+		
+		a = new ArrayList<>(d1.skills);
+		b = new ArrayList<>(d2.skills);
+		a.removeAll(d2.skills);
+		b.removeAll(d1.skills);
+		int distinctSkills = a.size() + b.size();
+		
+		return sharedSkills * distinctSkills;
+	}
+	
+	/**
+	 * Calcola il bonus potential
+	 */
+	private static int calcolaBP(Dipendente d1, Dipendente d2) {
+		if(d1.company.equals(d2.company)) {
+			return d1.bonus * d2.bonus;
+		}
+		return 0;
+	}
+	
+	/**
+	 * Calcola il total potential
+	 */
+	private static int calcolaTP(Dipendente d1, Dipendente d2) {
+		return calcolaBP(d1, d2)
+				+ (d1 instanceof Developer && d2 instanceof Developer?
+							calcolaWP((Developer)d1, (Developer)d2)
+						: 	0);
+	}
+	
 }
