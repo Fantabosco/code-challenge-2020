f7f81151522ee103b6615ab2555879fa6d039fdb
giovannipessiva <rpt@altervista.org>
2020-03-12 16:33:22 +0100

    Problem statement

--- src/main/java/it/reply/challenge/Main.java
+++ src/main/java/it/reply/challenge/Main.java
@@ -5,11 +5,18 @@ import java.util.List;
 import it.reply.challenge.utils.FileUtils;
 
 public class Main {
-
+	
+	private static final String INPUT_A = "a_solar.txt";
+	private static final String INPUT_B = "b_dream.txt";
+	private static final String INPUT_C = "c_soup.txt";
+	private static final String INPUT_D = "d_maelstrom.txt";
+	private static final String INPUT_E = "e_igloos.txt";
+	private static final String INPUT_F = "f_glitch.txt";
+	
 	public static void main(String[] args) {
 
 		// Reader
-		String dataset = null;
+		String dataset = INPUT_A;
 		List<String> file = FileUtils.readFile("in/" + dataset);
 		
 		// Parser
